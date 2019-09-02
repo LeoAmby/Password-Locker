a5210f53d42526b4cacc3d5ec8638dc9a395f067
Leo <leoamby@gmail.com>
2019-09-02 06:20:25 +0300

    creates a test file for the credentials class and imports unittest alongside the class

---
+++ Password-Locker/credentials_tests.py
@@ -0,0 +1,11 @@
+import unittest 
+from credentials import Credentials
+
+class TestUser(unittest.TestCase):
+
+    '''
+    This is a test class which defines various cases for the credentials class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class helps in creating test cases
+    '''
