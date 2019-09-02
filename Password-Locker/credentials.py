3410fc5538012b03ba7e74982df8aa656eaece7a
Leo <leoamby@gmail.com>
2019-09-02 11:56:16 +0300

    create tests for the credentials class, ran three of them where two failed

--- Password-Locker/credentials.py
+++ Password-Locker/credentials.py
@@ -2,7 +2,7 @@ class Credentials:
     '''
     This is a class that helps generate new instances of the user credentials
     '''
-    credentials = []
+    credentials_details = []
     def __init__(self,account_type,login,password):
         self.account_type = account_type
         self.login = login
--- Password-Locker/credentials_tests.py
+++ Password-Locker/credentials_tests.py
@@ -1,20 +1,51 @@
 import unittest 
 from credentials import Credentials
 
-class TestUser(unittest.TestCase):
-
+class TestCredentials(unittest.TestCase):
     '''
-    This is a test class which defines various cases for the credentials class behaviours.
-
+    This is a test class which defines various cases for the credential class behaviours.
     Args:
         unittest.TestCase: TestCase class helps in creating test cases
     '''
-
-    #Check if items are being instantiated corectly
-
     def setUp(self):
         '''
-        run before each test cases
+        Set up method to run before each test cases.
+        '''
+        self.new_credentials = Credentials("Instagram","waivery","!£$%67nghe")
+
+    def test_init(self):
+        '''
+        test_init test proper initialization of the credential objects or instances
         '''
 
-        self.new_credentials = Credentials("Instagram", "waiver", mwamtoleo@gmail.com)
+        self.assertEqual(self.new_credentials.account_type,"Instagram")
+        self.assertEqual(self.new_credentials.login,"waivery")
+        self.assertEqual(self.new_credentials.password,"!£$%67nghe")
+
+    def test_save_credentials(self):
+        '''
+        test_save_credentials test for clarity that the instances of the credentials are saved and can be found within the object
+        '''
+
+        self.new_credentials.save_credentials()
+
+        self.assertEqual(len(Credentials.credentials_details),1)
+    
+    def tearDown(self):
+        '''
+        This is our method that will aid in cleaning up every case that has completed running
+        '''
+
+        Credentials.credentials_details = []
+    
+    
+    def test_delete_credentials(self):
+
+        '''
+        test_delete_credentials for identifying whether we are in a position to remove or clear a specific credentials
+        '''
+        Credentials.credentials_details.remove(self)
+
+
+if __name__ == '__main__':
+        unittest.main()
