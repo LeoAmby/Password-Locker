1f1ccf4a1e785371616f16f1a42d74432dfccae8
Leo <leoamby@gmail.com>
2019-09-02 05:36:20 +0300

    made changes to the user class document

--- Password-Locker/user.py
+++ Password-Locker/user.py
@@ -2,8 +2,9 @@ class User:
     '''
     This is a class in which we shall create new instances for the user
     '''
-    def __init__(self,account_name,username,password):
+    user_details = []
+    def __init__(self,account_name,user_id,email):
 
         self.account_name = account_name
-        self.username = username
-        self.password = password
+        self.user_id = user_id
+        self.email = email
