100a4c3d59c973af9d97fc151330decdaf77fd36
Leo <leoamby@gmail.com>
2019-09-02 06:12:54 +0300

    create a user test file in which we import the user information and test the program

---
+++ Password-Locker/user_test.py
@@ -0,0 +1,11 @@
+import unittest 
+from user import User
+
+class TestUser(unittest.TestCase):
+
+    '''
+    This is a test class which defines various cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class helps in creating test cases
+    '''
