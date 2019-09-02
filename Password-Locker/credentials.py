f5877f536310b4d8663286cc1c890e82c59c1d3a
Leo <leoamby@gmail.com>
2019-09-02 05:59:51 +0300

    makes corrections on the highlighted errors

--- Password-Locker/credentials.py
+++ Password-Locker/credentials.py
@@ -1,8 +1,9 @@
-class Credentials
-'''
-This is a class that helps generate new instances of the user credentials
-'''
-def __init__(self,account_name,login,password):
-    self.account_name
-    self.login
-    self.password
+class Credentials:
+    '''
+    This is a class that helps generate new instances of the user credentials
+    '''
+    credentials = []
+    def __init__(self,account_name,login,password):
+        self.account_name
+        self.login
+        self.password
