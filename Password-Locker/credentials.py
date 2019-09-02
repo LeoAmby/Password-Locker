af904664fc993d47dcb85f5a5c6605daaf08d6ea
Leo <leoamby@gmail.com>
2019-09-02 05:56:00 +0300

    defines the instances in the credetial class

--- Password-Locker/credentials.py
+++ Password-Locker/credentials.py
@@ -1,1 +1,8 @@
-c
+class Credentials
+'''
+This is a class that helps generate new instances of the user credentials
+'''
+def __init__(self,account_name,login,password):
+    self.account_name
+    self.login
+    self.password
