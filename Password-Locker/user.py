83481b210f1dfb72ffa353fffe46c6dd191f1e74
Leo <leoamby@gmail.com>
2019-09-02 04:48:08 +0300

    elaborate on the instance descriptions which include the account_name, username and password

---
+++ Password-Locker/user.py
@@ -0,0 +1,9 @@
+class User:
+    '''
+    This is a class in which we shall create new instances for the user
+    '''
+    def __init__(self,account_name,username,password):
+
+        self.account_name = account_name
+        self.username = username
+        self.password = password
